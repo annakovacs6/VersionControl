c6b3a54bbf8db9c4bbb276a85b3d43e2b9c446da
Anna Kovács <anna.kovacs6@stud.uni-corvinus.hu>
2020-10-16 16:18:18 +0200

    #6. feladat - Adatok megjelenítése diagramon commit

--- week06/week06/Form1.cs
+++ week06/week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using week06.Entities;
 using week06.MnbServiceReference;
@@ -23,6 +24,7 @@ namespace week06
 			InitializeComponent();
 			CallWebService();
 			ProcessXML();
+			CreateChart();
 
 			dataGridView1.DataSource = Rates;
 		}
@@ -65,5 +67,26 @@ namespace week06
 				}
 			}
 		}
+
+		private void CreateChart()
+		{
+			chartRateData.DataSource = Rates;
+
+			var series = chartRateData.Series[0];
+			series.ChartType = SeriesChartType.Line;
+			series.XValueMember = "Date";
+			series.YValueMembers = "Value";
+
+			series.BorderWidth = 2;
+
+			var legend = chartRateData.Legends[0];
+			legend.Enabled = false;
+
+			var chartArea = chartRateData.ChartAreas[0];
+			chartArea.AxisX.MajorGrid.Enabled = false;
+			chartArea.AxisY.MajorGrid.Enabled = false;
+			chartArea.AxisY.IsStartedFromZero = false;
+		}
+
 	}
 }
