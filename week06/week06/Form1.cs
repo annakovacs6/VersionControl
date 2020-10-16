21f5ff493f3a3aa60b2365a29334dc5519f157c6
Anna Kovács <anna.kovacs6@stud.uni-corvinus.hu>
2020-10-16 15:54:19 +0200

    #4. feladat - Árfolyam lista létrehozása commit

--- week06/week06/Form1.cs
+++ week06/week06/Form1.cs
@@ -7,17 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06.Entities;
 using week06.MnbServiceReference;
 
 namespace week06
 {
 	public partial class Form1 : Form
 	{
+		BindingList<RateData> Rates = new BindingList<RateData>();
 		
 		public Form1()
 		{
 			InitializeComponent();
 			CallWebService();
+
+			dataGridView1.DataSource = Rates;
 		}
 
 		private void CallWebService()
