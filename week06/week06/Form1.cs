7f3f9c5c932d0bdf64d5809afc881f6c78b13fe5
Anna Kovács <anna.kovacs6@stud.uni-corvinus.hu>
2020-10-16 16:06:58 +0200

    #5.feladat - XML feldolgozása commit

--- week06/week06/Form1.cs
+++ week06/week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using week06.Entities;
 using week06.MnbServiceReference;
 
@@ -15,11 +16,13 @@ namespace week06
 	public partial class Form1 : Form
 	{
 		BindingList<RateData> Rates = new BindingList<RateData>();
-		
+		private string result;
+
 		public Form1()
 		{
 			InitializeComponent();
 			CallWebService();
+			ProcessXML();
 
 			dataGridView1.DataSource = Rates;
 		}
@@ -37,6 +40,30 @@ namespace week06
 
 			var response = mnbService.GetExchangeRates(request);
 			var result = response.GetExchangeRatesResult;
+		}
+
+		private void ProcessXML()
+		{
+			var xml = new XmlDocument();
+			xml.LoadXml(result);
+
+			foreach (XmlElement element in xml.DocumentElement)
+			{
+				var rate = new RateData();
+				Rates.Add(rate);
+
+				rate.Date = DateTime.Parse(element.GetAttribute("date");
+
+				var childElement = (XmlElement)element.ChildNodes[0];
+				rate.Currency = childElement.GetAttribute("curr");
+
+				var unit = decimal.Parse(childElement.GetAttribute("unit"));
+				var value = decimal.Parse(childElement.InnerText);
+				if (unit !=0)
+				{
+					rate.Value = value / unit;
+				}
+			}
 		}
 	}
 }
