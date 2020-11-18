8a0bc109824cbf8652ab85f8a949d7dd5890ce46
Anna Kovács <anna.kovacs6@stud.uni-corvinus.hu>
2020-11-18 18:41:47 +0100

    #7.feladat - Jelszó validálás teszt commit

--- UnitTestExample.Test/AccountControllerTestFixure.cs
+++ UnitTestExample.Test/AccountControllerTestFixure.cs
@@ -15,7 +15,7 @@ namespace UnitTestExample.Test
 			TestCase("abcd1234", false),
 			TestCase("irf@uni-corvinus", false),
 			TestCase("irf.uni-corvinus.hu", false),
-			TestCase("irf@uni-corvinus.hu", true)
+			TestCase("irf@uni-corvinus.hu", true),
 		]
 		public void TestValidateEmail(string email, bool expectedResult)
 		{
@@ -24,6 +24,26 @@ namespace UnitTestExample.Test
 
 			// Act
 			var actualResult = accountController.ValidateEmail(email);
+
+			// Assert
+			Assert.AreEqual(expectedResult, actualResult);
+		}
+
+		[
+			Test,
+			TestCase("abcDefg", false),
+			TestCase("ABCD1234", false),
+			TestCase("abcd1234", false),
+			TestCase("abCd123", false),
+			TestCase("ABcd1234", true)
+		]
+		public void TestValidatePassword(string password, bool expectedResult)
+		{
+			// Arrange
+			var accountController = new AccountController();
+
+			// Act
+			var actualResult = accountController.ValidatePassword(password);
 
 			// Assert
 			Assert.AreEqual(expectedResult, actualResult);
